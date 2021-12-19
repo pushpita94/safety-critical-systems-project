1b13d35cad72e2857291408db3052e6ef46040b0
Pushpita Sarkar <pushpita94@gmail.com>
2021-12-19 22:46:41 +0100

    report folder changed

---
+++ predictive-maintenance /src/test/java/de/frauas/predictivemaintenance/PredictiveMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.frauas.predictivemaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictiveMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
