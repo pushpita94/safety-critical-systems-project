7e27165bdb83cf48091d8f1b4aaccb54bc65e54d
Pushpita Sarkar <pushpita94@gmail.com>
2021-12-19 23:03:27 +0100

    api started

---
+++ predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/ApplicationRunner.java
@@ -0,0 +1,11 @@
+package de.frauas.predictivemaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
