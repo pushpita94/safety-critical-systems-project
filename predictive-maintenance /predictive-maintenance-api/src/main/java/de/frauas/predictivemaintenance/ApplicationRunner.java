1561eb2016338c631933b3f75707e5f3616504e0
Pushpita Sarkar <pushpita94@gmail.com>
2021-12-24 21:39:18 +0100

    URL problem fixed

--- predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/ApplicationRunner.java
+++ predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/ApplicationRunner.java
@@ -2,8 +2,9 @@ package de.frauas.predictivemaintenance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
