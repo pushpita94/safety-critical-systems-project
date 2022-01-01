0df7642adb5f386a634013b953736c384c24d577
Pushpita Sarkar <pushpita94@gmail.com>
2022-01-01 22:33:42 +0100

    logger added

--- predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/Controller/OpenApiController.java
+++ predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/Controller/OpenApiController.java
@@ -1,5 +1,9 @@
 package de.frauas.predictivemaintenance.Controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.catalina.util.ServerInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +12,26 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
 
 @RestController
+@Api(value = "OpenAPIHomepageController", description = "Openly available for all")
 public class OpenApiController {
+
+    private static final Logger LOGGER = Logger.getLogger(OpenApiController.class.getName());
+
+    @ApiOperation(value = "Welcome", notes = "Checks whether the API is up or not")
+    @ApiResponses(value = {
+            @ApiResponse(code= 200, message = "Checking whether the API is up or not", response = OpenApiController.class, responseContainer = "String"),
+            @ApiResponse(code = 401, message = "You are not authorized to view this resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String welcome(HttpServletRequest request) {
-        System.out.println("Predictive Maintenance API Base GET s called");
+        System.out.println("Predictive Maintenance API Base GET is called");
+        LOGGER.info("Predictive Maintenance API Base GET is called");
         return "<h2>Predictive Maintenance API</h2>" +
                 "Welcome to Pushu's API. Enjoy your stay!! <br/><br/>" +
                 ServerInfo.getServerBuilt() + "<br/>" +
