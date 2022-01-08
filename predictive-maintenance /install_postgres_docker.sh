6f364218a12ba9530565d19e5a85274f5534dec6
Pushpita Sarkar <pushpita94@gmail.com>
2022-01-08 15:07:54 +0100

    fixed port added for postgres

--- predictive-maintenance /install_postgres_docker.sh
+++ predictive-maintenance /install_postgres_docker.sh
@@ -1,2 +1,2 @@
 docker pull pushpita94/predictive_postgres:latest
-docker run --rm -P --name postgres pushpita94/predictive_postgres:latest
+docker run --rm -p 55432:5432 --name postgres pushpita94/predictive_postgres:latest
