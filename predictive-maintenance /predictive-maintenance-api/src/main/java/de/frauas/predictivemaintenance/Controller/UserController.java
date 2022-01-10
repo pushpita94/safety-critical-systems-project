c7e4a53a9fe5f820c6318667db76e1be8fe73e54
Pushpita Sarkar <pushpita94@gmail.com>
2022-01-10 20:20:36 +0100

    end point added

--- predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/Controller/UserController.java
+++ predictive-maintenance /predictive-maintenance-api/src/main/java/de/frauas/predictivemaintenance/Controller/UserController.java
@@ -3,7 +3,8 @@ package de.frauas.predictivemaintenance.Controller;
 import de.frauas.predictivemaintenance.Model.User;
 import de.frauas.predictivemaintenance.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -19,16 +20,22 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @RequestMapping(value = "/user/all", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<User> getAllUsers() {
         Iterable<User> allUsers = userRepository.findAll();
         return allUsers;
     }
 
-    public boolean doesUsernameExist(String username) {
+    @RequestMapping(value = "/user/username/{username}/exist", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public boolean doesUsernameExist(@PathVariable String username) {
         return userRepository.existsByUsername(username);
     }
 
-    public User getUserByUsername(String username) {
+    @RequestMapping(value = "/user/getDetails/{username}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserByUsername(@PathVariable String username) {
         if (userRepository.existsByUsername(username)) {
             LOGGER.info("Username " + username + "found! Fetching details..");
             return userRepository.findByUsername(username).get(0);
